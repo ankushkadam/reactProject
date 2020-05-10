6129ec559fd0abb933ea07f199d0208ce6789821
Ankush Anant Kadam <nkshkdm@gmail.com>
2020-05-10 15:20:41 +0530

    Updates

---
+++ precache-manifest.f5ace793854c2178b489a428ad88026c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb78fcdb6d1680ac4e75f6e4462e6e21",
+    "url": "/reactproject/index.html"
+  },
+  {
+    "revision": "732d01415df338d35ef1",
+    "url": "/reactproject/static/css/main.5553d068.chunk.css"
+  },
+  {
+    "revision": "707688742d1c5c6c3adc",
+    "url": "/reactproject/static/js/2.b79fa4b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactproject/static/js/2.b79fa4b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "732d01415df338d35ef1",
+    "url": "/reactproject/static/js/main.7170a589.chunk.js"
+  },
+  {
+    "revision": "4d2007254412030904ad",
+    "url": "/reactproject/static/js/runtime-main.1e0715a3.js"
+  }
+]);
